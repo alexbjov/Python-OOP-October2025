ec689f0a17785268462bd989828df0ead13380ae
alexbj <ajoveski@gmail.com>
2025-11-13 21:04:01 +0100

    Correct if/else block of the notification service

--- 07_solid_principles/notification_system.py
+++ 07_solid_principles/notification_system.py
@@ -2,48 +2,48 @@ from abc import ABC, abstractmethod
 
 
 class NotificationSender(ABC):
-	def __init__(self):
-		self.is_under_maintenance = False
-	
-	@abstractmethod
-	def send(self, message: str):
-		pass
-	
-	@property
-	def is_under_maintenance(self):
-		return self.__is_under_maintenance
-	
-	@is_under_maintenance.setter
-	def is_under_maintenance(self, value: bool):
-		if not isinstance(value, bool):
-			raise TypeError('The value must be a boolean!')
-		self.__is_under_maintenance = value
+    def __init__(self):
+        self.is_under_maintenance = False
+    
+    @abstractmethod
+    def send(self, message: str):
+        pass
+    
+    @property
+    def is_under_maintenance(self):
+        return self.__is_under_maintenance
+    
+    @is_under_maintenance.setter
+    def is_under_maintenance(self, value: bool):
+        if not isinstance(value, bool):
+            raise TypeError('The value must be a boolean!')
+        self.__is_under_maintenance = value
 
 
 class EmailSender(NotificationSender):
-	def send(self, message: str):
-		print(f"Sending email with message: {message}")
+    def send(self, message: str):
+        print(f"Sending email with message: {message}")
 
 
 class SMSSender(NotificationSender):
-	def send(self, message: str):
-		print(f"Sending SMS with message: {message}")
+    def send(self, message: str):
+        print(f"Sending SMS with message: {message}")
 
 
 class PushSender(NotificationSender):
-	def send(self, message: str):
-		print(f"Sending Push with message: {message}")
+    def send(self, message: str):
+        print(f"Sending Push with message: {message}")
 
 
 class NotificationService:
-	def __init__(self, sender: NotificationSender):
-		self.sender = sender
-	
-	def notify(self, message: str):
-		if self.sender.is_under_maintenance:
-			print('This service is under maintenance.')
-			return
-		self.sender.send(message)
+    def __init__(self, sender: NotificationSender):
+        self.sender = sender
+    
+    def notify(self, message: str):
+        if self.sender.is_under_maintenance:
+            print('This service is under maintenance.')
+        else:
+            self.sender.send(message)
 
 
 email_sender = EmailSender()
@@ -52,7 +52,8 @@ push_sender = PushSender()
 
 push_sender.is_under_maintenance = True
 """
-The status change represents manual toggling done by an admin or a developer when they know a system component is under maintenance.
+The status change represents manual toggling done by an admin or a developer when they know a system component is
+under maintenance.
 In more complex systems, this would be automated or pulled from an external source.
 """
 email_service = NotificationService(email_sender)
